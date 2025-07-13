82146725b74d5470c1274eb01896910cd67cf918
Fedorich <102855254+FedoRich-P@users.noreply.github.com>
2025-07-13 13:23:14 +0300

    Ref

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), 'VITE_')
+export default defineConfig(() => {
 
     return {
         plugins: [
@@ -12,12 +11,13 @@ export default defineConfig(({ mode }) => {
         ],
         server: {
             proxy: {
-                "/socket.io": {
-                    target: env.VITE_API_URL || 'http://localhost:5000', // Fallback
+                '/socket.io': {
+                    target: 'https://chat-websocket-3k7r.onrender.com',
+                    changeOrigin: true,
                     ws: true,
-                    changeOrigin: true
-                },
-            },
+                    secure: true
+                }
+            }
         },
     }
 })
