79944c5f5ae2202601c702a98ffbde8fe0c2861f
Fedorich <102855254+FedoRich-P@users.noreply.github.com>
2025-07-13 13:06:08 +0300

    Ref

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,22 +1,23 @@
-import {defineConfig} from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite"
 
-// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), 'VITE_')
 
-export default defineConfig({
-    plugins: [
-        react(),
-        tailwindcss()
-    ],
-    server: {
-        proxy: {
-            "/socket.io": {
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                    // @ts-expect-error
-                target: import.meta.env.VITE_API_URL,
-                ws: true,
+    return {
+        plugins: [
+            react(),
+            tailwindcss()
+        ],
+        server: {
+            proxy: {
+                "/socket.io": {
+                    target: env.VITE_API_URL || 'http://localhost:5000', // Fallback
+                    ws: true,
+                    changeOrigin: true
+                },
             },
         },
-    },
+    }
 })
